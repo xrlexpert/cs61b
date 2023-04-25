96c0e74f0dc3d6a131c5fefdc85843eed86eddc0
xrlexpert <hiroxrl@outlook.com>
2023-04-25 22:52:35 +0800

    finish lab02-boomb

--- lab02/src/bomb/BombMain.java
+++ lab02/src/bomb/BombMain.java
@@ -11,13 +11,19 @@ public class BombMain {
         // TODO: Find the correct passwords to each phase using debugging techniques
         Bomb b = new Bomb();
         if (phase >= 0) {
-            b.phase0("Figure this out. I wonder where the phases are defined...");
+            b.phase0("39291226");
         }
         if (phase >= 1) {
-            b.phase1(null); // Figure this out too
+            b.phase1(IntList.of(0,9,3,0,8)); // Figure this out too
         }
         if (phase >= 2) {
-            b.phase2("Figure this out. I wonder where the phases are defined...");
+            String s="";
+            for(int i=0;i<=1338;i++)
+            {
+                s=s+i;
+                s+= " ";
+            }
+            b.phase2(s);
         }
     }
 }
